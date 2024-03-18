9c67580011f6f0584c7b87a0fa39e1bf245ff949
LAPTOP-LVL8JE5O\LEGION <1305780485@qq.com>
2024-03-18 12:33:14 +0800

    完成编写

--- shp-relation-xls/index.js
+++ shp-relation-xls/index.js
@@ -6,7 +6,6 @@ const { GeoJson2Shp } = require("@gis-js/geojson2shp");
 const JSZip = require("jszip");
 const compressing = require("compressing");
 
-const filePath = path.resolve("oldFiles");
 // --------------------------------------------
 /**
  * 参数
@@ -15,7 +14,8 @@ const encodings = { "2023涉河建筑物": "utf-8", "2022年涉河建筑物": "G
 const shpRelationId = "code"; //shp的关联id
 const xlsTableHeaderIndex = 1; //excel字段起始列
 const xlsRelationId = "BM"; //excel的关联id
-const deleteFields = []; //结果里需要删除的字段
+const deleteFields = []; //原始数据需要删除的字段
+const addFields = ["XMMC"]; //添加的字段，"*"代表xls的所有字段
 const toStringFields = ["code", "经度", "纬度", "年份"]; //转成字符串的字段
 const replaceFields = {
   市: "S",
@@ -24,10 +24,24 @@ const replaceFields = {
   河流: "RV_NAME",
   名称: "XMMC",
   类型: "LXMC",
-};
-
+}; //替换字段对应关系
+const sortFields = [
+  "XMMC",
+  "市",
+  "县",
+  "显隐",
+  "名称",
+  "类型",
+  "经度",
+  "纬度",
+  "位置",
+  "河流",
+  "备注",
+  // "code",
+  // "年份",
+]; //排序字段
 // --------------------------------------------
-
+const filePath = path.resolve("oldFiles");
 getFile(filePath, true);
 const folderName = "newFiles";
 function getFile(filePath, init) {
@@ -81,7 +95,7 @@ function handlerFiles(names, filePath) {
           const features = [];
           source.read().then(function log(result) {
             if (result.done) {
-              console.log("shp done");
+              console.log(`${i} shp done`);
               shapefile
                 .openDbf(filePath + "/" + dbf, {
                   encoding: encodings[i] ? encodings[i] : "utf-8",
@@ -90,8 +104,7 @@ function handlerFiles(names, filePath) {
                   const properties = [];
                   source.read().then(async function log(result) {
                     if (result.done) {
-                      console.log("dbf done");
-
+                      console.log(`${i} dbf done`);
                       const sheets = xlsx.parse(filePath + "/" + xls);
                       const sheet = sheets[0];
                       if (sheet?.data) {
@@ -111,6 +124,7 @@ function handlerFiles(names, filePath) {
                             result[temp[relationIndex]] = obj;
                           }
                         });
+                        console.log(`${i} xls done`);
 
                         features.forEach((data, i) => {
                           if (
@@ -121,7 +135,6 @@ function handlerFiles(names, filePath) {
                             deleteFields.forEach((field) => {
                               delete properties[i][field];
                             });
-
                             for (let item in properties[i]) {
                               if (replaceFields[item]) {
                                 //替换数据
@@ -130,12 +143,36 @@ function handlerFiles(names, filePath) {
                               }
                             }
                             data.properties = properties[i];
+
+                            //追加字段
+                            if (addFields.indexOf("*") > -1) {
+                              //所有字段
+                              data.properties = {
+                                ...data.properties,
+                                ...result[bmValue],
+                              };
+                            } else {
+                              addFields.forEach((field) => {
+                                data.properties[field] = result[bmValue][field];
+                              });
+                            }
+
                             //转换字段类型
                             toStringFields.forEach((field) => {
                               if (data.properties[field] !== undefined)
                                 data.properties[field] =
                                   data.properties[field]?.toString();
                             });
+
+                            //排序字段
+                            const sortResult = {};
+                            sortFields.forEach((field) => {
+                              sortResult[field] = data.properties[field];
+                            });
+                            data.properties = {
+                              ...sortResult,
+                              ...data.properties,
+                            };
                           } else {
                             console.log("属性错乱！");
                           }
@@ -158,7 +195,7 @@ function handlerFiles(names, filePath) {
                             if (err) {
                               console.error(err);
                             } else {
-                              console.log("geojson success");
+                              console.log(`${i} generate geojson success`);
                             }
                           }
                         );
@@ -169,9 +206,59 @@ function handlerFiles(names, filePath) {
 
                         const projection =
                           'GEOGCS["GCS_China_Geodetic_Coordinate_System_2000",DATUM["D_China_2000",SPHEROID["CGCS2000",6378137.0,298.257222101]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]]';
+                        g2s.writePoint(
+                          function (err, files) {
+                            const fileName = i + "_point";
+                            layers.file(fileName + ".shp", files.shp.buffer, {
+                              binary: true,
+                            });
+                            layers.file(fileName + ".shx", files.shx.buffer, {
+                              binary: true,
+                            });
+                            layers.file(fileName + ".dbf", files.dbf.buffer, {
+                              binary: true,
+                            });
+                            layers.file(fileName + ".prj", projection);
+                            layers.file(fileName + ".cpg", cpg);
+                          }.bind(this)
+                        );
+
+                        g2s.writeMultiPoint(
+                          function (err, files) {
+                            const fileName = i + "_multipoint";
+                            layers.file(fileName + ".shp", files.shp.buffer, {
+                              binary: true,
+                            });
+                            layers.file(fileName + ".shx", files.shx.buffer, {
+                              binary: true,
+                            });
+                            layers.file(fileName + ".dbf", files.dbf.buffer, {
+                              binary: true,
+                            });
+                            layers.file(fileName + ".prj", projection);
+                            layers.file(fileName + ".cpg", cpg);
+                          }.bind(this)
+                        );
+
+                        g2s.writePolyline(
+                          function (err, files) {
+                            const fileName = i + "_polyline";
+                            layers.file(fileName + ".shp", files.shp.buffer, {
+                              binary: true,
+                            });
+                            layers.file(fileName + ".shx", files.shx.buffer, {
+                              binary: true,
+                            });
+                            layers.file(fileName + ".dbf", files.dbf.buffer, {
+                              binary: true,
+                            });
+                            layers.file(fileName + ".prj", projection);
+                            layers.file(fileName + ".cpg", cpg);
+                          }.bind(this)
+                        );
                         g2s.writePolygon(
                           function (err, files) {
-                            var fileName = i;
+                            const fileName = i + "_polygon";
                             layers.file(fileName + ".shp", files.shp.buffer, {
                               binary: true,
                             });
@@ -194,12 +281,13 @@ function handlerFiles(names, filePath) {
                               content,
                               async function (err) {
                                 if (!err) {
+                                  console.log(`${i} zip success`);
                                   compressing.zip
                                     .uncompress(zipPath, folderName)
                                     .then(() => {
-                                      console.log("unzip success");
+                                      console.log(`${i} unzip success`);
                                       fs.unlinkSync(zipPath);
-                                      console.log("zip  delete success");
+                                      console.log(`${i} deletezip success`);
                                     })
                                     .catch((err) => {
                                       console.log(err);
@@ -211,7 +299,6 @@ function handlerFiles(names, filePath) {
                             );
                           });
                       }
-
                       return;
                     }
                     properties.push(result.value);
--- shp-relation-xls/readme.md
+++ shp-relation-xls/readme.md
@@ -10,3 +10,37 @@
 # 运行
 `npm start`
 
+
+#  参数说明
+```javascript
+const encodings = { "2023涉河建筑物": "utf-8", "2022年涉河建筑物": "GBK" }; //shp的编码
+const shpRelationId = "code"; //shp的关联id
+const xlsTableHeaderIndex = 1; //excel字段起始列
+const xlsRelationId = "BM"; //excel的关联id
+const deleteFields = []; //原始数据需要删除的字段
+const addFields = ["XMMC"]; //添加的字段，"*"代表xls的所有字段
+const toStringFields = ["code", "经度", "纬度", "年份"]; //转成字符串的字段
+const replaceFields = {
+  市: "S",
+  县: "X",
+  位置: "WZ",
+  河流: "RV_NAME",
+  名称: "XMMC",
+  类型: "LXMC",
+}; //替换字段对应关系
+const sortFields = [
+  "XMMC",
+  "市",
+  "县",
+  "显隐",
+  "名称",
+  "类型",
+  "经度",
+  "纬度",
+  "位置",
+  "河流",
+  "备注",
+  // "code",
+  // "年份",
+]; //排序字段
+```
